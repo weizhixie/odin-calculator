a23feceaad664ea221cc805228a884437140f448
weizhixie <wx02697@yahoo.co.uk>
2023-08-24 16:44:39 +0100

    Add basic add, subtract, multiply, and divide math operation

---
+++ scripts.js
@@ -0,0 +1,35 @@
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+function operate(firstNumber, operator, secondNumber) {
+    let result = 0;
+    switch (operator) {
+        case "+":
+            result = add(firstNumber, secondNumber);
+            break;
+        case "-":
+            result = subtract(firstNumber, secondNumber);
+            break;
+        case "*":
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            result = divide(firstNumber, secondNumber);
+            break;
+    }
+
+    return result;
+}
